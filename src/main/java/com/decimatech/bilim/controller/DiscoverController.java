47d20b92369931229119d61c5649cba6a232430d
Fatih Erdem <93.fatiherdem@gmail.com>
2016-02-17 18:14:29 +0200

    Pi'ler icin IP Discover servisi eklendi

--- src/main/java/com/decimatech/bilim/controller/DiscoverController.java
+++ src/main/java/com/decimatech/bilim/controller/DiscoverController.java
@@ -5,6 +5,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Controller
 @RequestMapping(value = "/discover")
@@ -24,8 +29,17 @@ public class DiscoverController {
     @RequestMapping(value = "", method = RequestMethod.POST)
     public ResponseEntity<String> discoverStations(@RequestBody Station station, HttpServletRequest request){
 
-        String ipAddress = request.getRemoteAddr();
+        Station foundStation = mongoTemplate.findOne(query(where("stationId").is(station.getStationId())), Station.class);
 
-        return null;
+        if(foundStation != null){
+            String ipAddress = request.getRemoteAddr();
+            foundStation.setStationIp(ipAddress);
+            mongoTemplate.save(foundStation);
+            log.info("\n <CODE 200> " + foundStation.toString());
+            return new ResponseEntity<String>(HttpStatus.OK);
+        }else {
+            log.info("\n <CODE 404> IP Adresi Eklenemedi!!!");
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+        }
     }
 }
